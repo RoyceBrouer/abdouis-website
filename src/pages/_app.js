d932589ee593d0b61b99cd7ebd9fd268b830cdb6
Royce Brouër <royce@systemli.org>
2023-10-16 14:52:44 +0200

    initial homepage styling added root colors try sizing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+// import GlobalStyle from "../styles";
+// import useSWR, { SWRConfig } from "swr";
+// import Layout from "@/components/Layout";
+import { useState } from "react";
+// import { uid } from "uid";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // const { data, isLoading, error } = useSWR(
+  //   `temporary-resources/images`,
+  //   fetcher
+  // );
+
+  return <Component {...pageProps} />;
 }
