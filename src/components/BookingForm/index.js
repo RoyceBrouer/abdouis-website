33eecae5c695530788e391a039e49dbe5b7b340b
Royce Brouër <royce@systemli.org>
2023-10-28 23:11:24 +0200

    correct import BookingForm.module.css

--- src/components/BookingForm/index.js
+++ src/components/BookingForm/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Form.module.css";
+import styles from "./BookingForm.module.css";
 
 export default function Form({ onSubmit, inEnglish }) {
   async function handleSubmit(event) {
