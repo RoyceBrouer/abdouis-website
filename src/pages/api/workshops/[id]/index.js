bea6f3269b16922dc0cca7bd9761c92a5c3dc018
Royce Brouër <royce@systemli.org>
2023-11-02 13:12:24 +0100

    push again coz why not

--- src/pages/api/workshops/[id]/index.js
+++ src/pages/api/workshops/[id]/index.js
@@ -6,45 +6,42 @@ import Workshop from "../../../../../db/models/Workshop";
 export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
-
   console.log("REQUEST QUERY", request.query);
   console.log("id from request query", id);
-  if (!id) {
-    return;
-  }
+
   if (request.method === "GET") {
     const workshop = await Workshop.findById(id).populate("images");
     if (!workshop) {
-      return response.status(404).json({ status: "Not found" });
+      response.status(404).json({ status: "Not found" });
     }
     response.status(200).json(workshop);
   }
 
-  if (request.method === "PUT") {
-    try {
-      const { titleEnglish, titleGerman, textEnglish, textGerman } = req.body;
+  // if (request.method === "PUT") {
+  //   try {
+  //     const { titleEnglish, titleGerman, textEnglish, textGerman } = req.body;
 
-      await Workshop.findById(id);
+  //     await Workshop.findById(id);
 
-      const workshop = await Workshop.findById(id);
+  //     const workshop = await Workshop.findById(id);
 
-      if (!workshop) {
-        return response.status(404).json({ error: "Document not found" });
-      }
+  //     if (!workshop) {
+  //       return response.status(404).json({ error: "Document not found" });
+  //     }
 
-      workshop.titleEnglish = titleEnglish;
-      workshop.titleGerman = titleGerman;
-      workshop.textEnglish = textEnglish;
-      workshop.textGerman = textGerman;
+  //     workshop.titleEnglish = titleEnglish;
+  //     workshop.titleGerman = titleGerman;
+  //     workshop.textEnglish = textEnglish;
+  //     workshop.textGerman = textGerman;
 
-      await document.save();
+  //     await document.save();
 
-      response.status(200).json({ message: "Document updated successfully" });
-    } catch (error) {
-      console.error(error);
-      response.status(500).json({ error: "Internal server error" });
-    }
-  } else {
-    response.status(405).json({ error: "Method not allowed" });
-  }
+  //     response.status(200).json({ message: "Document updated successfully" });
+  //   } catch (error) {
+  //     console.error(error);
+  //     response.status(500).json({ error: "Internal server error" });
+  //   }
+  // } else {
+  //   response.status(405).json({ error: "Method not allowed" });
+  // }
 }
